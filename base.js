08580af18954030089a573a9cd1a545801aa1112
vassilis <vas.terzo@gmail.com>
2015-04-03 22:33:23 +0300

    working on keyboard interactivity

--- base.js
+++ base.js
@@ -8,12 +8,28 @@ $body = el.body;
 el.addEventListener("keydown", onkeydown, true);
 el.addEventListener("keyup", onkeyup, true);
 
-function onkeydown(e){
+function onkeydown(e) {
+    // console.log(e.which);   
+    var key_pressed = el.getElementById("key-" + e.which);
+    if (key_pressed !== null) {
+        $body.classList.add("key-pressed");
+        key_pressed.classList.add("pressed");
+    }
     if (e.shiftKey) {
-        $body.classList.add("shift_is_pressed");
+        $body.classList.add("shift-pressed");
     } 
 }            
 
-function onkeyup(e){
-    $body.removeAttribute("class");
+function onkeyup(e) {
+    // console.log(e.which); 
+    var key_pressed = el.getElementById("key-" + e.which);
+    if (e.which == 16) {
+      $body.classList.remove("shift-pressed");
+    }
+    if (key_pressed !== null) {
+            key_pressed.classList.remove("pressed");
+            if (el.querySelectorAll(".pressed").length == 0) {
+                    $body.classList.remove("key-pressed");
+            }
+    }
 }            
