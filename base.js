62d7756afa37352f116265ca818fa16adf0e0384
vassilis <vas.terzo@gmail.com>
2015-04-04 07:07:44 +0300

    fixed formatting issues

--- base.js
+++ base.js
@@ -1,5 +1,4 @@
 
-
 var el, $body;
 
 el = document;
@@ -9,27 +8,27 @@ el.addEventListener("keydown", onkeydown, true);
 el.addEventListener("keyup", onkeyup, true);
 
 function onkeydown(e) {
-    // console.log(e.which);   
-    var key_pressed = el.getElementById("key-" + e.which);
-    if (key_pressed !== null) {
-        $body.classList.add("key-pressed");
-        key_pressed.classList.add("pressed");
-    }
-    if (e.shiftKey) {
-        $body.classList.add("shift-pressed");
-    } 
-}            
+  console.log(e.which);
+  var key_pressed = el.getElementById("key-" + e.which);
+  if (key_pressed !== null) {
+    $body.classList.add("key-pressed");
+    key_pressed.classList.add("pressed");
+  }
+  if (e.shiftKey) {
+    $body.classList.add("shift-pressed");
+  } 
+}
 
 function onkeyup(e) {
-    // console.log(e.which); 
-    var key_pressed = el.getElementById("key-" + e.which);
-    if (e.which == 16) {
-      $body.classList.remove("shift-pressed");
+  // console.log(e.which); 
+  var key_pressed = el.getElementById("key-" + e.which);
+  if (e.which == 16) {
+    $body.classList.remove("shift-pressed");
+  }
+  if (key_pressed !== null) {
+    key_pressed.classList.remove("pressed");
+    if (el.querySelectorAll(".pressed").length == 0) {
+      $body.classList.remove("key-pressed");
     }
-    if (key_pressed !== null) {
-            key_pressed.classList.remove("pressed");
-            if (el.querySelectorAll(".pressed").length == 0) {
-                    $body.classList.remove("key-pressed");
-            }
-    }
-}            
+  }
+}
